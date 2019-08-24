c47864bbd880c6f423176c923d521ccc3bd02967
Ing Julio C. Ocaña <julio.ocana@bitsamericas.com>
2019-08-24 02:12:16 -0400

    Adding vue to project

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const client = require('./services/redis-client');
 const path = require('path');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
